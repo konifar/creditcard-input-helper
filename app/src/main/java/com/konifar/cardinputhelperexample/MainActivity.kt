20009cac1259dbbc5119e797791fbef88de4c9bc
konifar <yahpeycoy0403@gmail.com>
2020-01-30 09:30:24 +0900

    Change package

--- app/src/main/java/com/konifar/cardinputhelperexample/MainActivity.kt
+++ app/src/main/java/com/konifar/cardinputhelperexample/MainActivity.kt
@@ -1,14 +1,17 @@
-package com.konifar.cardinputhelper
+package com.konifar.cardinputhelperexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.konifar.cardinputhelper.R
 import com.konifar.cardinputhelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
+            R.layout.activity_main
+        )
     }
 }
