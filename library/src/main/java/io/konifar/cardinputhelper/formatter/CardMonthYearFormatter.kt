d83da1abc281f6fad604085fb38a4a2a5918e498
konifar <yahpeycoy0403@gmail.com>
2020-02-06 03:30:57 +0900

    Fixed error

--- library/src/main/java/io/konifar/cardinputhelper/formatter/CardMonthYearFormatter.kt
+++ library/src/main/java/io/konifar/cardinputhelper/formatter/CardMonthYearFormatter.kt
@@ -5,17 +5,56 @@ import io.konifar.cardinputhelper.ext.digits
 object CardMonthYearFormatter {
     const val SLASH = "/"
 
-    fun format(yearMonth: CharSequence): String {
-        return when (yearMonth.toString()) {
-            SLASH -> ""
-            "2", "3", "4", "5", "6", "7", "8", "9" -> "0$yearMonth$SLASH"
-            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" -> "$yearMonth$SLASH"
-            else -> {
-                if (yearMonth.length >= 4 && yearMonth.last().toString() == SLASH) {
-                    return StringBuilder(yearMonth.digits()).insert(2, SLASH).toString()
+    fun format(monthYear: CharSequence): String {
+        if (monthYear.isEmpty()) {
+            return ""
+        }
+
+        val monthYearArray = monthYear.split(SLASH)
+        // Slash is not included
+        if (monthYearArray.size == 1) {
+            if (monthYear.length == 1) {
+                return if (monthYear.digits().toInt() <= 1) {
+                    monthYear.toString()
+                } else {
+                    "0$monthYear$SLASH"
                 }
-                return yearMonth.toString()
+            }
+            if (monthYear.length >= 2) {
+                return if (monthYear.digits().toInt() <= 12) {
+                    "$monthYear$SLASH"
+                } else {
+                    StringBuilder("0$monthYear").insert(2, SLASH).toString()
+                }
             }
         }
+
+        // Slash is included
+        if (monthYearArray.size == 2) {
+            val month = monthYearArray.first()
+            val year = monthYearArray.last()
+
+            if (month.isEmpty()) {
+                return if (year.isEmpty()) { // "/"
+                    ""
+                } else {
+                    val endIndex = (monthYear.length).coerceAtMost(3)
+                    monthYear.substring(0, endIndex)
+                }
+            }
+
+            val modifiedMonthYear = when {
+                month.toInt() < 10 && !month.startsWith("0") -> "0${monthYear.digits()}"
+                else -> monthYear.digits()
+            }
+
+            if (modifiedMonthYear.length > 2) {
+                val result = StringBuilder(modifiedMonthYear).insert(2, SLASH).toString()
+                val endIndex = (result.length).coerceAtMost(5)
+                return result.substring(0, endIndex)
+            }
+        }
+
+        return monthYear.toString()
     }
 }
---
+++ library/src/test/java/io/konifar/cardinputhelper/formatter/CardMonthYearFormatterTest.kt
@@ -0,0 +1,52 @@
+package io.konifar.cardinputhelper.formatter
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class CardMonthYearFormatterTest(
+    private val input: String,
+    private val output: String
+) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): List<Array<out Any?>> {
+            return listOf(
+                arrayOf("", ""),
+                arrayOf("0", "0"),
+                arrayOf("1", "1"),
+                arrayOf("2", "02/"),
+                arrayOf("3", "03/"),
+                arrayOf("9", "09/"),
+                arrayOf("10", "10/"),
+                arrayOf("12", "12/"),
+                arrayOf("13", "01/3"),
+                arrayOf("13", "01/3"),
+                arrayOf("/", ""),
+                arrayOf("01/", "01/"),
+                arrayOf("01/2", "01/2"),
+                arrayOf("01/23", "01/23"),
+                arrayOf("123/", "12/3"),
+                arrayOf("1/", "01/"),
+                arrayOf("1/2", "01/2"),
+                arrayOf("1/23", "01/23"),
+                arrayOf("1/234", "01/23"),
+                arrayOf("12/34", "12/34"),
+                arrayOf("123/4", "12/34"),
+                arrayOf("1234/", "12/34"),
+                arrayOf("/1", "/1"),
+                arrayOf("/12", "/12"),
+                arrayOf("/123", "/12")
+            )
+        }
+    }
+
+    @Test
+    fun extract() {
+        assertEquals(output, CardMonthYearFormatter.format(input))
+    }
+}
