0230270a2e31b04610dbc22723eb71caf6470c7a
konifar <yahpeycoy0403@gmail.com>
2020-02-11 10:39:11 +0900

    Add simple text formatter

---
+++ library/src/main/java/io/konifar/cardinputhelper/CardNumber4digitsFormatTextWatcher.kt
@@ -0,0 +1,28 @@
+package io.konifar.cardinputhelper
+
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+
+class CardNumber4digitsFormatTextWatcher : TextWatcher {
+    private var current = ""
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+    override fun afterTextChanged(s: Editable) {
+        if (s.toString() != current) {
+            val userInput = s.toString().replace(nonDigits, "")
+            if (userInput.length <= 16) {
+                current = userInput.chunked(4).joinToString(" ")
+                s.filters = arrayOfNulls<InputFilter>(0)
+            }
+            s.replace(0, s.length, current, 0, current.length)
+        }
+    }
+
+    companion object {
+        private val nonDigits = Regex("[^\\d]")
+    }
+}
