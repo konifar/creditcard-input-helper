bec1a581c1caf3e241d87799c7e81df337c2c843
konifar <yahpeycoy0403@gmail.com>
2020-02-11 10:57:35 +0900

    Added custom selection action mode callback

---
+++ library/src/main/java/io/konifar/cardinputhelper/NoCopyAndCutSelectionActionModeCallback.kt
@@ -0,0 +1,26 @@
+package io.konifar.cardinputhelper
+
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.IdRes
+
+class NoCopyAndCutSelectionActionModeCallback : ActionMode.Callback {
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        removeItemIfNeed(menu, android.R.id.copy)
+        removeItemIfNeed(menu, android.R.id.cut)
+        return true
+    }
+
+    private fun removeItemIfNeed(menu: Menu?, @IdRes id: Int) {
+        if (menu?.findItem(id) != null) {
+            menu.removeItem(id)
+        }
+    }
+
+    override fun onActionItemClicked(mode: ActionMode?, menu: MenuItem?) = false
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?) = false
+
+    override fun onDestroyActionMode(mode: ActionMode?) = Unit
+}
