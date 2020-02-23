cd51dbd09f1eab2496f5772c4ff38c723d430b76
konifar <yahpeycoy0403@gmail.com>
2020-02-23 11:32:42 +0900

    Support api version 21

--- library/src/main/java/io/konifar/cardinputhelper/cardbrand/Mastercard.kt
+++ library/src/main/java/io/konifar/cardinputhelper/cardbrand/Mastercard.kt
@@ -5,7 +5,6 @@ import java.util.regex.Pattern
 /**
  * 5555 5555 5555 4444
  * 2223 0031 2200 3222
- * 2210-2720
  */
 class Mastercard(
     override val brandPattern: Pattern = Pattern.compile("^(5[1-5][0-9]{2}|2[2-7][0-9]{2})$"),
